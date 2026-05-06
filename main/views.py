b88d6156916101a269c90580eee8a492f0ba99ce
creosciant <lastnameispiccolo@gmail.com>
2026-05-06 13:08:46 +0000

    Version 1.0

--- main/views.py
+++ main/views.py
@@ -1,7 +1,16 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'static/typeshift/index.html')
+    return render(request, 'index.html')
+
+def rankings(request, genre=None):
+    return render(request, 'rankings.html', {'genre': genre})
+
+def about(request):
+    return render(request, 'about.html')
+
+def api_scores(request):
+    return JsonResponse({'scores': []})
