8da7ed723201c2b380eaa7e8c4e47090d6f2d095
KurakuJaeger <apecschoolclark@gmail.com>
2026-05-06 16:44:04 +0800

    2026-5-6 : Vercel 404 fixing

--- main/views.py
+++ main/views.py
@@ -1,7 +1,4 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-
 def index(request):
-    return HttpResponse("Hello from main app")
+    return render(request, 'main/index.html')
