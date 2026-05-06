3e5841e7b2c29da53000bb39f7c60308e30b3880
KurakuJaeger <apecschoolclark@gmail.com>
2026-05-06 17:04:02 +0800

    garbage

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import index
+from .views import index, rankings, about, api_scores
 
 urlpatterns = [
     path("", index, name="main.index"),
+    path("rankings/", rankings, name="main.rankings"),
+    path("rankings/<str:genre>/", rankings, name="main.rankings_genre"),
+    path("about/", about, name="main.about"),
+    path("api/scores/", api_scores, name="api.scores"),
 ]
